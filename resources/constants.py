f9a5ab1229c23b93ba403a0350993b78bb599e83
elhor <diego.elhor@gmail.com>
2024-04-23 19:35:02 +0200

    Still refactoring

--- resources/constants.py
+++ resources/constants.py
@@ -1,2 +1,2 @@
-TITLE = 'Wallettraker v.1.0 by elhorDev\t\t\t\t\t'
+TITLE = 'Wallettraker v.1.0 by elhorDev'
 URL = "https://www.productoscotizados.com/mercado/ibex-35"
--- view/walletui.py
+++ view/walletui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import time
 from tkinter import ttk
 from controller.realtime import urlcontent, scrapurl, realtime
 from resources.constants import TITLE, URL
@@ -15,13 +16,23 @@ class AppUi(tk.Tk):
         self.geometry('1920x1080')
         self.resizable(False, False)
 
+        self.menu_bar()
         self.mostrar_datos_tabulares()
+        self.scale_refresh_rate()
+        self.frame_label()
+        self.show_wallet_data()
+        self.frame_label_sell_buy_opcions()
+        self.clock_refresh()
+    @staticmethod
+    def show_tiempo_real():
+        result = urlcontent(URL)
+        scrapurl(result)
 
+    def menu_bar(self):
         # Crear la barra de menu.
         menu_bar = tk.Menu(self)
 
         # Creamos menu archivo
-
         archivo_menu = tk.Menu(menu_bar, tearoff=False)
         archivo_menu.add_command(label='Cargar cartera')
         archivo_menu.add_command(label='Guardar cartera')
@@ -32,6 +43,7 @@ class AppUi(tk.Tk):
         menu_bar.add_cascade(label='Archivo', menu=archivo_menu)
         self.config(menu=menu_bar)
 
+    def frame_label(self):
         # Creamos frame para el label y el reloj
         frame_superior = tk.Frame(self)
 
@@ -40,48 +52,34 @@ class AppUi(tk.Tk):
         cabecera.config(font=('Terminal', 25))
         cabecera.grid(row=0, column=0)
 
-        # Creamos el label para el reloj.
-        reloj = tk.Label(frame_superior, text='')
-        reloj.grid(row=0, column=1, columnspan=1)
-
         frame_superior.grid(row=0, column=0)
 
+    def frame_label_sell_buy_opcions(self):
         # Creamos frame para opciones de compra y venta.
-
         frame_opciones = tk.Frame(self)
         frame_opciones.grid(row=1, column=1)
-        # Variable de control para los Radiobutton de las opciones.
 
+        # Variable de control para los Radiobutton de las opciones.
         opcion = tk.StringVar(value='venta')
         opcion_tobin = tk.BooleanVar()
 
         # Variables de control
-
         control_opcion = tk.IntVar()
         control_qty = tk.IntVar()
         control_expense = tk.DoubleVar()
         control_price = tk.DoubleVar()
         control_index = tk.IntVar()
 
-        # Creamos un tk.scale para controlar la actualizacion.
-
-        freq_actualizacion_var_control = tk.IntVar(value=10)
-
-        label_slide = tk.Label(frame_opciones, text='\n\nFrecuencia de actualizacion', font=('Terminal', 10))
-        label_bajo_slide = tk.Label(frame_opciones, text='Segundos', font=('Terminal', 8))
-        slide = tk.Scale(frame_opciones, from_=3, to=20, orient='horizontal', resolution=1,
-                         variable=freq_actualizacion_var_control, sliderlength=10)  #command=control_freq_actualizacion
-
         # Creamos opciones dentro de frame.
         opcion_radio_tobin = tk.Checkbutton(frame_opciones, text='Tobin', font=('Terminal', 16), variable=opcion_tobin)
         opcion_venta = tk.Radiobutton(frame_opciones, text='Venta ', font=('Terminal', 16), variable=opcion,
                                       value='venta'''',
-                                      command=color_boton''')
+                                              command=color_boton''')
         opcion_venta.pack()
 
         opcion_compra = tk.Radiobutton(frame_opciones, text='Compra ', font=('Terminal', 16), variable=opcion,
                                        value='compra'''',
-                                       command=color_boton''')
+                                               command=color_boton''')
         opcion_compra.pack()
 
         label_stock = tk.Label(frame_opciones, text='Valor: ', font=('Terminal', 16))
@@ -98,7 +96,7 @@ class AppUi(tk.Tk):
         entry_price = tk.Entry(frame_opciones, textvariable=control_price)
 
         boton_ejecutar = tk.Button(frame_opciones, text='Ejecutar', font=('Terminal', 14), bg='green'
-                                   )  #,command=aniadir_compra
+                                   )  # ,command=aniadir_compra
 
         label_stock.pack(padx=10, pady=5)
         entry_stock.pack(padx=10, pady=5)
@@ -117,19 +115,31 @@ class AppUi(tk.Tk):
 
         boton_ejecutar.pack(padx=10, pady=5)
 
+    def scale_refresh_rate(self):
+        # Creamos un tk.scale para controlar la actualizacion.
+        frame_scale = tk.Frame(self)
+        frame_scale.grid(row=2, column=1)
+
+        freq_actualizacion_var_control = tk.IntVar(value=10)
+
+        label_slide = tk.Label(frame_scale, text='\n\nFrecuencia de actualizacion', font=('Terminal', 10))
+        label_bajo_slide = tk.Label(frame_scale, text='Segundos', font=('Terminal', 8))
+        slide = tk.Scale(frame_scale, from_=3, to=20, orient='horizontal', resolution=1,
+                         variable=freq_actualizacion_var_control, sliderlength=10)  # command=control_freq_actualizacion
+        '''
         label_slide.pack()
         slide.pack()
         label_bajo_slide.pack()
+        '''
+
+    def show_wallet_data(self):
 
         # Creamos tabla de wallet.
-
         wallet_tabular = ttk.Treeview(self, height=35)
-
         wallet_tabular.grid(row=3, column=0)
 
         # Label wallet
         label_wallet = tk.Label(self, text='Wallet Personal', font=('Terminal', 20))
-
         label_wallet.grid(row=2, column=0)
 
     def mostrar_datos_tabulares(self):
@@ -138,9 +148,7 @@ class AppUi(tk.Tk):
 
         columns = ['Stock', 'Precio', 'Hora', 'Var', 'Close', 'Differencia']
         realtime_tabular = ttk.Treeview(self, height=35)
-
         realtime_tabular.grid(row=1, column=0)
-
         realtime_tabular['columns'] = columns
 
         for col in columns:
@@ -153,13 +161,20 @@ class AppUi(tk.Tk):
         for stock in realtime:
             realtime_tabular.insert('', 'end', values=(stock.stock, stock.realtime_price, stock.time,
                                                        stock.var, stock.close, stock.more_or_less))
-        print('Actualizando tiempo real cada {} segundos')
+        print('Actualizando tiempo real cada 5 segundos')
 
         self.after(5000, func=self.mostrar_datos_tabulares)
 
-    def show_tiempo_real(self):
-        result = urlcontent(URL)
-        scrapurl(result)
+    def clock_refresh(self):
+        # Creamos el label para el reloj.
+        # Con su actualizacion
+        clock_frame = tk.Frame()
+        clock_frame.grid(row=0, column=1)
+        clock = tk.Label(clock_frame, text='')
+        clock.grid(row=0, column=1, columnspan=1)
+        clock.config(text=time.strftime('\t%H:%M:%S'), font=('Terminal', 25))
+
+        self.after(1000, self.clock_refresh)
 
 
 app = AppUi()
